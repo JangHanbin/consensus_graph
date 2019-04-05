2f510f814f8faa112f97dfcca96888f63d9838ad
장한빈 <janghanbin@janghanbin-ui-MacBook-Pro.local>
2019-04-05 11:39:07 +0900

    modified

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ def ncr(n, r):
     r = min(r, n-r)
     numer = reduce(op.mul, range(n, n-r, -1), 1)
     denom = reduce(op.mul, range(1, r+1), 1)
-    global COUNT
-    return int(numer // denom)
+
+    return Decimal(numer) / Decimal(denom)
 
 def confirm(q, z):
     p = Decimal(1.0) - q
@@ -36,25 +36,42 @@ def safety_of_consensus(a, max_of_validator, num_of_nodes):
     z_values = dict()
 
     for m in max_of_validator:
-        sum = 0
-        for i in range(math.ceil(m / 4), int(min(m, num_of_nodes * a / 100)) + 1):
-
+        sum = Decimal(0)
+        # int(max(math.ceil(m / 4), m-(1-(a/100))*num_of_nodes))
+        for i in range(max(math.ceil(m / 4), round(m-(1-a/100)*n)), int(min(m, num_of_nodes * a / 100)) + 1):
             # if there is a pre value then use that nor calculate
-            x = x_values.get(i) if x_values.get(i) else ncr(int(num_of_nodes * a / 100), i)
-            y = y_values.get(m - i) if y_values.get(m - i) else ncr(int(num_of_nodes * (1 - (a / 100))), m - i)
+            x = x_values.get(i) if x_values.get(i) else ncr(round(num_of_nodes * a / 100), i)
+            y = y_values.get(m - i) if y_values.get(m - i) else ncr(round(num_of_nodes * (1 - (a / 100))), m - i)
             z = z_values.get(m) if z_values.get(m) else ncr(num_of_nodes, m)
             x_values.update({i: x})
             y_values.update({m - i: y})
             z_values.update({m: z})
 
-            result = x * y / z
+            result = Decimal(x) * Decimal(y) / Decimal(z)
+            # if a==68:
+            #     print('M : {0} I : {1}'.format(m,i))
+            #     print(int(m-(1-a/100)*n))
+            #     print(m - (1 - a / 100) * n)
+            #     print('{0} ncr {1} = {2}'.format(int(num_of_nodes * a / 100),i,x))
+            #     print('{0} ncr {1} = {2}'.format(int(num_of_nodes * (1 - (a / 100))), m-i, y))
+            #     print('{0} ncr {1} = {2}'.format(num_of_nodes, m, z))
+
             sum += result
+            # if a == 68:
+            #     print(sum)
+            #     print()
+
+
 
         # insert by percentage of safety
-
-        results.append((1 - sum) * 100)
+        results.append((1- sum) * 100)
+        # print(sum)
+        # print(int((Decimal(1) - sum)))
+        # print((1 - sum) * 100)
+        # print('m[{0}] = {1},'.format(m,float((1- sum) * 100)))
 
     return results
+
 
 def possibility_of_propagation(p, max_of_validator, num_of_nodes, idx, ret_dict):
 
@@ -68,7 +85,7 @@ def possibility_of_propagation(p, max_of_validator, num_of_nodes, idx, ret_dict)
         sum = 0
         print('{0} of {1} processing in Propagation[{2}] \r'.format(m, max(max_of_validator),p),end='')
 
-        for i in range(math.ceil(m / 4), int(min(m, num_of_nodes * (1 - (p / 100))))+1):
+        for i in range(max(math.ceil(m / 4), m-p*n), int(min(m, num_of_nodes * (1 - (p / 100))))+1):
             x = x_values.get(m - i) if x_values.get(m - i) else ncr(int(num_of_nodes * p / 100), m - i)
             y = y_values.get(i) if y_values.get(i) else ncr(int(num_of_nodes * (1 - (p / 100))), i)
             z = z_values.get(m) if z_values.get(m) else ncr(num_of_nodes, m)
@@ -93,23 +110,74 @@ def possibility_of_propagation(p, max_of_validator, num_of_nodes, idx, ret_dict)
 if __name__=='__main__':
 
     n = 8954
+    # n = 100
     # rate of attacker
     a = 10
     # rate of propagation
     p_list = [32, 60, 74, 80]
+    # p_list = [i for i in range(1,11)]
 
     max_of_validator = range(1, n + 1)
     plt.grid(True)
     colors = ['#C80000', '#001EFF', '#FFE600', '#00C800']
 
+    a_list=list()
+
+    #for
+    for p in p_list:
+        tmp = list()
+        for i in range(100-p, min(100-p+a+1,100)):
+            tmp.append(i)
+        a_list.append(tmp)
 
     # Safety
-    safety = safety_of_consensus(a, max_of_validator, n)
+    safeties = list()
+    for a_ in a_list:
+        for a in a_:
+            safety = safety_of_consensus(a, max_of_validator, n)
+            safeties.append(safety.copy())
+
+
+        # plt.plot(max_of_validator, safety)
+
+    # for safety in safeties:
+    #     print(safety)
+
+    # # for
+    for _a in a_list:
+        for a, safety in zip(_a, safeties):
+            plt.plot(max_of_validator, safety)
+            print(a , safety)
+            excelSaver = ExcelSaver('safety_value_node_{0}_propagation_{1}.xlsx'.format(max(max_of_validator), a))
+            excelSaver.save_to_file(max_of_validator, safety)
+
+
+
     plt.ylabel('Safety [%]')
     plt.xlabel('Number of miners')
-
+    plt.show()
+    exit(0)
+    # for idx in range(0, len(safeties)):
+    #     sum =0
+    #     for safety in safeties:
+    #         # print(safety[idx])
+    #         sum += safety[idx]
+    #     print(sum / len(safeties))
+    #
+    # total=list()
+    # for safety in safeties:
+    #     for j in range(0, len(safety)):
+    #         sum = 0
+    #         for idx in range(0, len(safeties)):
+    #             # print('safeties[{0}][{1}]'.format(idx,j))
+    #             sum += safeties[idx][j]
+    #         total.append(sum/len(safeties))
+    #     break
+    # print(total)
+    # plt.plot(max_of_validator,total)
+    # plt.show()
     # draw main line
-    # plt.plot(max_of_validator, safety, c='k')
+
     # print('Safety of consensus')
     # for idx, i in enumerate(p_list):
     #     i= i/100
@@ -117,6 +185,7 @@ if __name__=='__main__':
     #     y = int(safety[int(i*max(max_of_validator))])
     #     plt.plot(x, y ,linestyle=None, ms=15, c=colors[idx], marker='*')
         # plt.annotate('({0}, {1})'.format(x,y), xy=(x,y), xytext=(x-700, y-(2*idx)))
+
 
 
 
@@ -177,13 +246,13 @@ if __name__=='__main__':
         # draw star at each p
         plt.plot(int(p*max(max_of_validator)), result[int(p*max(max_of_validator))-1], linestyle=None, ms=15, c=color, marker='*')
         # point of star
-        print('{2} : x = {0}, y = {1}'.format( int(p*max(max_of_validator)),result[int(p*max(max_of_validator))-1], p ))
-        # To find 99.97 safety by confirm
-        q = 1 - (result[int(p*max(max_of_validator))-1] / 100)
-        for z in range(0,100):
-            if 99.97 < confirm(q=q, z=z):
-                print('Z : {0} value = {1}'.format(z,confirm(q=q, z=z)))
-                break
+        # print('{2}% : x = {0}, y = {1}'.format( int(p*max(max_of_validator)),result[int(p*max(max_of_validator))-1], p*100 ))
+        # # To find 99.97 safety by confirm
+        # q = 1 - (result[int(p*max(max_of_validator))-1] / 100)
+        # for z in range(0,100):
+        #     if 99.97 < confirm(q=q, z=z):
+        #         print('Z : {0} value = {1}'.format(z,confirm(q=q, z=z)))
+        #         break
 
     # plt.title('Confirm')
     # plt.ylabel('Value [%]')
